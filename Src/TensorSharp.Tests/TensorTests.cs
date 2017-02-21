319040c87b1d26058716db4b15201fb07b99c401
ajlopez <webmaster@ajlopez.com>
2017-02-21 18:18:06 -0300

    Tensor first creation with dimensions, WIP

---
+++ Src/TensorSharp.Tests/TensorTests.cs
@@ -0,0 +1,17 @@
+﻿namespace TensorSharp.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TensorTests
+    {
+        [TestMethod]
+        public void CreateTensorWithThreeDimensions()
+        {
+            Tensor tensor = new Tensor(3, 4, 5);
+
+            Assert.AreEqual(3, tensor.NoDimensions);
+        }
+    }
+}
---
+++ Src/TensorSharp/Tensor.cs
@@ -0,0 +1,19 @@
+﻿namespace TensorSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Tensor
+    {
+        private int[] dimensions;
+
+        public Tensor(params int[] dimensions)
+        {
+            this.dimensions = (int[])dimensions.Clone();
+        }
+
+        public int NoDimensions { get { return this.dimensions.Length; } }
+    }
+}
