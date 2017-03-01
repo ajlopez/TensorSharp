492c840c7bfb3b426b7f5547f8f4223ea1d17fde
ajlopez <webmaster@ajlopez.com>
2017-03-01 13:36:01 -0300

    Get/Set Value first implementation

--- Src/TensorSharp/Tensor.cs
+++ Src/TensorSharp/Tensor.cs
@@ -8,10 +8,17 @@
     public class Tensor<T>
     {
         private int[] dimensions;
+        private T[] values;
+        private int size;
 
         public Tensor(params int[] dimensions)
         {
             this.dimensions = (int[])dimensions.Clone();
+
+            this.size = 1;
+
+            for (int k = 0; k < dimensions.Length; k++)
+                this.size *= dimensions[k];
         }
 
         public int Rank { get { return this.dimensions.Length; } }
@@ -26,7 +33,10 @@
             if (coordinates.Any(c => c < 0))
                 throw new TensorException("Invalid coordinate");
 
-            return default(T);
+            if (this.values == null)
+                return default(T);
+
+            return this.values[this.CalculatePosition(coordinates)];
         }
 
         public void SetValue(T value, params int[] coordinates)
@@ -34,7 +44,17 @@
             if (coordinates.Any(c => c < 0))
                 throw new TensorException("Invalid coordinate");
 
-            throw new NotImplementedException();
+            if (this.values == null)
+                this.values = new T[this.size];
+
+            this.values[this.CalculatePosition(coordinates)] = value;
+        }
+
+        private int CalculatePosition(int[] coordinates)
+        {
+            int position = coordinates[0];
+
+            return position;
         }
     }
 }
