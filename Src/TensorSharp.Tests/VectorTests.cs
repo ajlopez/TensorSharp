ea23636a3c1efe6504adfad0aa9f4f4999ef7649
ajlopez <webmaster@ajlopez.com>
2017-07-17 11:16:56 -0300

    Vector first tests

---
+++ Src/TensorSharp.Tests/VectorTests.cs
@@ -0,0 +1,20 @@
+﻿namespace TensorSharp.Tests
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class VectorTests
+    {
+        [TestMethod]
+        public void CreateIntegerVector()
+        {
+            var vector = new Vector<int>(10);
+
+            Assert.AreEqual(1, vector.Rank);
+            Assert.IsNotNull(vector.Shape);
+            Assert.IsTrue(vector.Shape.SequenceEqual(new int[] { 10 }));
+        }
+    }
+}
---
+++ Src/TensorSharp/Vector.cs
@@ -0,0 +1,23 @@
+﻿namespace TensorSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Vector<T>
+    {
+        private T[] values;
+        private int[] shape;
+
+        public Vector(int length)
+        {
+            this.values = new T[length];
+            this.shape = new int[] { length };
+        }
+
+        public int Rank { get { return 1; } }
+
+        public int[] Shape { get { return this.shape; } }
+    }
+}
