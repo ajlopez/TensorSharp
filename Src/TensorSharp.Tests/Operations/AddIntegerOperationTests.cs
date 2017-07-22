9fd94d7a3ff38093e19d41b874773455d99e585d
ajlopez <webmaster@ajlopez.com>
2017-07-22 15:29:42 -0300

    AddIntegerOperation first test and implementation

---
+++ Src/TensorSharp.Tests/Operations/AddIntegerOperationTests.cs
@@ -0,0 +1,30 @@
+﻿namespace TensorSharp.Tests.Operations
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TensorSharp.Operations;
+
+    [TestClass]
+    public class AddIntegerOperationTests
+    {
+        [TestMethod]
+        public void AddSingleValues()
+        {
+            IValue<int> left = new SingleValue<int>(1);
+            IValue<int> right = new SingleValue<int>(41);
+
+            var add = new AddIntegerOperation(left, right);
+
+            Assert.AreEqual(left.Rank, add.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(right.Shape));
+
+            var result = add.Evaluate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(left.Rank, result.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(result.Shape));
+            Assert.AreEqual(42, result.GetValue());
+        }
+    }
+}
---
+++ Src/TensorSharp/Operations/AddIntegerOperation.cs
@@ -0,0 +1,37 @@
+﻿namespace TensorSharp.Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class AddIntegerOperation : IValue<int>
+    {
+        private IValue<int> left;
+        private IValue<int> right;
+        private int rank;
+        private int[] shape;
+
+        public AddIntegerOperation(IValue<int> left, IValue<int> right)
+        {
+            this.left = left;
+            this.right = right;
+            this.rank = left.Rank;
+            this.shape = left.Shape;
+        }
+
+        public int Rank { get { return this.rank; } }
+
+        public int[] Shape { get { return this.shape; } }
+
+        public int GetValue(params int[] coordinates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IValue<int> Evaluate()
+        {
+            return new SingleValue<int>(this.left.GetValue() + this.right.GetValue());
+        }
+    }
+}
