78351d8823b95b6fcc04a614df2d8bc3afef28c0
ajlopez <webmaster@ajlopez.com>
2017-09-11 12:00:51 -0300

    Subtract operation

---
+++ Src/TensorSharp.Tests/Operations/SubtractIntegerOperationTests.cs
@@ -0,0 +1,109 @@
+﻿namespace TensorSharp.Tests.Operations
+{
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TensorSharp.Operations;
+
+    [TestClass]
+    public class SubtractIntegerOperationTests
+    {
+        [TestMethod]
+        public void SubtractSingleValues()
+        {
+            INode<int> left = new SingleValue<int>(43);
+            INode<int> right = new SingleValue<int>(1);
+
+            var add = new SubtractIntegerOperation(left, right);
+
+            Assert.AreEqual(left.Rank, add.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(right.Shape));
+
+            var result = add.Evaluate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(left.Rank, result.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(result.Shape));
+            Assert.AreEqual(42, result.GetValue());
+        }
+
+        [TestMethod]
+        public void SubtractVectors()
+        {
+            INode<int> left = new Vector<int>(new int[] { 1, 2, 3 });
+            INode<int> right = new Vector<int>(new int[] { 4, 5, 6 });
+
+            var add = new SubtractIntegerOperation(left, right);
+
+            Assert.AreEqual(left.Rank, add.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(right.Shape));
+
+            var result = add.Evaluate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(left.Rank, result.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(result.Shape));
+            Assert.AreEqual(-3, result.GetValue(0));
+            Assert.AreEqual(-3, result.GetValue(1));
+            Assert.AreEqual(-3, result.GetValue(2));
+        }
+
+        [TestMethod]
+        public void SubtractVectorsWithDifferentLengths()
+        {
+            INode<int> left = new Vector<int>(new int[] { 1, 2, 3 });
+            INode<int> right = new Vector<int>(new int[] { 4, 5, 6, 7 });
+
+            try
+            {
+                new SubtractIntegerOperation(left, right);
+                Assert.Fail();
+            }
+            catch (InvalidOperationException ex)
+            {
+            }
+        }
+
+        [TestMethod]
+        public void SubtractMatrices()
+        {
+            INode<int> left = new Matrix<int>(new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 } });
+            INode<int> right = new Matrix<int>(new int[][] { new int[] { 10, 20, 30 }, new int[] { 40, 50, 60 } }); ;
+
+            var add = new SubtractIntegerOperation(left, right);
+
+            Assert.AreEqual(left.Rank, add.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(right.Shape));
+
+            var result = add.Evaluate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(left.Rank, result.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(result.Shape));
+
+            Assert.AreEqual(-9, result.GetValue(0, 0));
+            Assert.AreEqual(-18, result.GetValue(0, 1));
+            Assert.AreEqual(-27, result.GetValue(0, 2));
+
+            Assert.AreEqual(-36, result.GetValue(1, 0));
+            Assert.AreEqual(-45, result.GetValue(1, 1));
+            Assert.AreEqual(-54, result.GetValue(1, 2));
+        }
+
+        [TestMethod]
+        public void SubtractMatricesWithDifferentShapes()
+        {
+            INode<int> left = new Matrix<int>(new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 } });
+            INode<int> right = new Matrix<int>(new int[][] { new int[] { 10, 20 }, new int[] { 40, 50 } }); ;
+
+            try
+            {
+                new SubtractIntegerOperation(left, right);
+                Assert.Fail();
+            }
+            catch (InvalidOperationException ex)
+            {
+            }
+        }
+    }
+}
---
+++ Src/TensorSharp/Operations/SubtractIntegerOperation.cs
@@ -0,0 +1,76 @@
+﻿namespace TensorSharp.Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class SubtractIntegerOperation : INode<int>
+    {
+        private INode<int> left;
+        private INode<int> right;
+        private int rank;
+        private int[] shape;
+
+        public SubtractIntegerOperation(INode<int> left, INode<int> right)
+        {
+            if (left.Rank == 1 && right.Rank == 1)
+                if (left.Shape[0] != right.Shape[0])
+                    throw new InvalidOperationException();
+
+            if (left.Rank == 2 && right.Rank == 2)
+                if (left.Shape[0] != right.Shape[0] || left.Shape[1] != right.Shape[1])
+                    throw new InvalidOperationException();
+
+            this.left = left;
+            this.right = right;
+            this.rank = left.Rank;
+            this.shape = left.Shape;
+        }
+
+        public int Rank { get { return this.rank; } }
+
+        public int[] Shape { get { return this.shape; } }
+
+        public int GetValue(params int[] coordinates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public INode<int> Evaluate()
+        {
+            if (this.left.Rank == 1)
+            {
+                int[] newvalues = new int[this.left.Shape[0]];
+                int l = newvalues.Length;
+
+                for (int k = 0; k < l; k++)
+                    newvalues[k] = this.left.GetValue(k) - this.right.GetValue(k);
+
+                return new Vector<int>(newvalues);
+            }
+
+            if (this.left.Rank == 2)
+            {
+                int l = this.left.Shape[0];
+                int m = this.left.Shape[1];
+                int[][] newvalues = new int[l][];
+
+                for (int k = 0; k < l; k++)
+                {
+                    newvalues[k] = new int[m];
+
+                    for (int j = 0; j < m; j++)
+                        newvalues[k][j] = this.left.GetValue(k, j) - this.right.GetValue(k, j);
+                }
+
+                return new Matrix<int>(newvalues);
+            }
+
+            if (this.left.Rank == 0)
+                return new SingleValue<int>(this.left.GetValue() - this.right.GetValue());
+
+            return null;
+        }
+    }
+}
