f32db71d6bedae12f8dac328fed5d0107a009dc5
ajlopez <webmaster@ajlopez.com>
2017-09-12 14:12:45 -0300

    Flow Constant

---
+++ Src/TensorSharp.Tests/FlowTests.cs
@@ -0,0 +1,18 @@
+﻿namespace TensorSharp.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FlowTests
+    {
+        [TestMethod]
+        public void CreateConstantSingleValue()
+        {
+            INode<int> value = Flow.Constant<int>(42);
+
+            Assert.IsNotNull(value);
+            Assert.AreEqual(42, value.GetValue());
+        }
+    }
+}
---
+++ Src/TensorSharp/Flow.cs
@@ -0,0 +1,15 @@
+﻿namespace TensorSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public static class Flow
+    {
+        public static INode<T> Constant<T>(T value)
+        {
+            return new SingleValue<T>(value);
+        }
+    }
+}
