a240cab63b70f80f0e809dadf8daf7642b2fd87c
ajlopez <webmaster@ajlopez.com>
2017-10-22 12:25:22 -0300

    Refactor, using BaseValueNode for single value

---
+++ Src/TensorSharp/BaseValueNode.cs
@@ -0,0 +1,44 @@
+﻿namespace TensorSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class BaseValueNode<T> : INode<T>
+    {
+        private int[] shape;
+        private T[] values;
+
+        public BaseValueNode(int[] shape, T[] values)
+        {
+            this.shape = shape;
+            this.values = values;
+        }
+
+        public int[] Shape { get { return this.shape; } }
+
+        public T[] Values { get { return this.values; } }
+
+        public int Rank { get { return this.shape.Length; } }
+
+        public INode<T> Evaluate()
+        {
+            return this;
+        }
+
+        public T GetValue(params int[] coordinates)
+        {
+            int offset = 0;
+            int factor = 1;
+
+            for (int k = 0; k < this.shape.Length; k++)
+            {
+                offset += coordinates[k] * factor;
+                factor *= this.shape[k];
+            }
+
+            return this.values[offset];
+        }
+    }
+}
--- Src/TensorSharp/SingleValue.cs
+++ Src/TensorSharp/SingleValue.cs
@@ -5,28 +5,13 @@
     using System.Linq;
     using System.Text;
 
-    public class SingleValue<T> : INode<T>
+    public class SingleValue<T> : BaseValueNode<T>
     {
         private static int[] shape = new int[0];
-        private T value;
 
         public SingleValue(T value)
+            : base(shape, new T[] { value })
         {
-            this.value = value;
-        }
-
-        public int Rank { get { return 0; } }
-
-        public int[] Shape { get { return shape; } }
-
-        public T GetValue(params int[] coordinates)
-        {
-            return this.value;
-        }
-
-        public INode<T> Evaluate()
-        {
-            return this;
         }
     }
 }
