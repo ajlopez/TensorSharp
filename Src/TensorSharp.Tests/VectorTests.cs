632c15575b6529d5d3d910fc163baa1bedcf0991
ajlopez <webmaster@ajlopez.com>
2017-10-23 13:38:48 -0300

    Refactor, Vector extending BaseValueNode

--- Src/TensorSharp.Tests/VectorTests.cs
+++ Src/TensorSharp.Tests/VectorTests.cs
@@ -30,19 +30,5 @@
             Assert.AreEqual(2, vector.GetValue(1));
             Assert.AreEqual(3, vector.GetValue(2));
         }
-
-        [TestMethod]
-        public void SetAndGetValues()
-        {
-            var vector = new Vector<int>(3);
-
-            vector.SetValue(1, 0);
-            vector.SetValue(2, 1);
-            vector.SetValue(3, 2);
-
-            Assert.AreEqual(1, vector.GetValue(0));
-            Assert.AreEqual(2, vector.GetValue(1));
-            Assert.AreEqual(3, vector.GetValue(2));
-        }
     }
 }
--- Src/TensorSharp/Vector.cs
+++ Src/TensorSharp/Vector.cs
@@ -5,40 +5,16 @@
     using System.Linq;
     using System.Text;
 
-    public class Vector<T> : INode<T>
+    public class Vector<T> : BaseValueNode<T>
     {
-        private T[] values;
-        private int[] shape;
-
         public Vector(int length)
+            : base(new int[] { length }, new T[length])
         {
-            this.values = new T[length];
-            this.shape = new int[] { length };
         }
 
         public Vector(T[] values)
+            : base(new int[] { values.Length }, values)
         {
-            this.values = values;
-            this.shape = new int[] { values.Length };
-        }
-
-        public int Rank { get { return 1; } }
-
-        public int[] Shape { get { return this.shape; } }
-
-        public T GetValue(params int[] coordinates)
-        {
-            return this.values[coordinates[0]];
-        }
-
-        public void SetValue(T value, params int[] coordinates)
-        {
-            this.values[coordinates[0]] = value;
-        }
-
-        public INode<T> Evaluate()
-        {
-            return this;
         }
     }
 }
