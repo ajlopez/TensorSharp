956543cbd3a54f9959162591b3d01a69591b51a1
ajlopez <webmaster@ajlopez.com>
2017-11-01 07:00:02 -0300

    Flow.Constant use BaseValueNode to create a Matrix node

--- Src/TensorSharp/Flow.cs
+++ Src/TensorSharp/Flow.cs
@@ -20,7 +20,7 @@
 
         public static INode<T> Constant<T>(T[][] values)
         {
-            return new Matrix<T>(values);
+            return new BaseValueNode<T>(CalculateShape(values), ToFlatArray(values));
         }
 
         public static INode<int> Add(INode<int> left, INode<int> right)
@@ -41,6 +41,40 @@
         public static INode<double> Subtract(INode<double> left, INode<double> right)
         {
             return new SubtractDoubleOperation(left, right);
+        }
+
+        private static int[] CalculateShape<T>(T[][] values)
+        {
+            if (values == null)
+                throw new InvalidOperationException();
+
+            for (int k = 1; k < values.Length; k++)
+                if (values[k].Length != values[0].Length)
+                    throw new InvalidOperationException();
+
+            int[] shape = new int[2];
+
+            shape[0] = values.Length;
+            shape[1] = values[0].Length;
+
+            return shape;
+        }
+
+        private static T[] ToFlatArray<T>(T[][] values)
+        {
+            int[] shape = CalculateShape(values);
+            int nrows = shape[0];
+            int ncols = shape[1];
+
+            T[] flatvalues = new T[nrows * ncols];
+
+            int k = 0;
+
+            for (int x = 0; x < ncols; x++)
+                for (int y = 0; y < nrows; y++)
+                    flatvalues[k++] = values[y][x];
+
+            return flatvalues;
         }
     }
 }
