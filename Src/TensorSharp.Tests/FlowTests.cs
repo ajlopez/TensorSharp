38fb2fc9ed864ba41b87711a8fe3e9fd6a20a591
ajlopez <webmaster@ajlopez.com>
2017-12-09 11:15:42 -0300

    Additional multiply integer tests for Flow

--- Src/TensorSharp.Tests/FlowTests.cs
+++ Src/TensorSharp.Tests/FlowTests.cs
@@ -395,6 +395,72 @@
         }
 
         [TestMethod]
+        public void MultiplyIntegerSingleValues()
+        {
+            INode<int> left = Flow.Constant(2);
+            INode<int> right = Flow.Constant(21);
+
+            var multiply = Flow.Multiply(left, right);
+
+            Assert.AreEqual(left.Rank, multiply.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(right.Shape));
+
+            var result = multiply.Evaluate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(left.Rank, result.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(result.Shape));
+            Assert.AreEqual(42, result.GetValue());
+        }
+
+        [TestMethod]
+        public void MultiplyIntegerVectors()
+        {
+            INode<int> left = Flow.Constant(new int[] { 1, 2, 3 });
+            INode<int> right = Flow.Constant(new int[] { 4, 5, 6 });
+
+            var multiply = Flow.Multiply(left, right);
+
+            Assert.AreEqual(left.Rank, multiply.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(right.Shape));
+
+            var result = multiply.Evaluate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(left.Rank, result.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(result.Shape));
+            Assert.AreEqual(4, result.GetValue(0));
+            Assert.AreEqual(10, result.GetValue(1));
+            Assert.AreEqual(18, result.GetValue(2));
+        }
+
+        [TestMethod]
+        public void MultiplyIntegerMatrices()
+        {
+            INode<int> left = Flow.Constant(new int[][] { new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 } });
+            INode<int> right = Flow.Constant(new int[][] { new int[] { 10, 20, 30 }, new int[] { 40, 50, 60 } });
+
+            var multiply = Flow.Multiply(left, right);
+
+            Assert.AreEqual(left.Rank, multiply.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(right.Shape));
+
+            var result = multiply.Evaluate();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(left.Rank, result.Rank);
+            Assert.IsTrue(left.Shape.SequenceEqual(result.Shape));
+
+            Assert.AreEqual(10, result.GetValue(0, 0));
+            Assert.AreEqual(40, result.GetValue(0, 1));
+            Assert.AreEqual(90, result.GetValue(0, 2));
+
+            Assert.AreEqual(160, result.GetValue(1, 0));
+            Assert.AreEqual(250, result.GetValue(1, 1));
+            Assert.AreEqual(360, result.GetValue(1, 2));
+        }
+
+        [TestMethod]
         public void DivideIntegerSingleValues()
         {
             INode<int> left = Flow.Constant(84);
