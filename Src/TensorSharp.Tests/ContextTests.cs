1b21dd49f6aed2196368439e2fb0ec4557357ef9
ajlopez <webmaster@ajlopez.com>
2017-12-23 11:57:16 -0300

    Context

---
+++ Src/TensorSharp.Tests/ContextTests.cs
@@ -0,0 +1,60 @@
+﻿namespace TensorSharp.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TensorSharp.Nodes;
+
+    [TestClass]
+    public class ContextTests
+    {
+        [TestMethod]
+        public void GetUnknowIntegerNode()
+        {
+            Context context = new Context();
+
+            Assert.IsNull(context.GetNode<int>("x"));
+        }
+
+        [TestMethod]
+        public void GetUnknowDoubleNode()
+        {
+            Context context = new Context();
+
+            Assert.IsNull(context.GetNode<double>("y"));
+        }
+
+        [TestMethod]
+        public void SetAndGetIntegerNode()
+        {
+            Context context = new Context();
+
+            INode<int> node = Flow.Constant<int>(42);
+
+            context.SetNode("x", node);
+
+            var result = context.GetNode<int>("x");
+
+            Assert.IsNotNull(result);
+            Assert.AreSame(node, result);
+
+            Assert.IsNull(context.GetNode<double>("x"));
+        }
+
+        [TestMethod]
+        public void SetAndGetDoubleNode()
+        {
+            Context context = new Context();
+
+            INode<double> node = Flow.Constant<double>(3.14159);
+
+            context.SetNode("x", node);
+
+            var result = context.GetNode<double>("x");
+
+            Assert.IsNotNull(result);
+            Assert.AreSame(node, result);
+
+            Assert.IsNull(context.GetNode<int>("x"));
+        }
+    }
+}
---
+++ Src/TensorSharp/Context.cs
@@ -0,0 +1,26 @@
+﻿namespace TensorSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using TensorSharp.Nodes;
+
+    public class Context
+    {
+        private Dictionary<string, object> nodes = new Dictionary<string, object>();
+
+        public void SetNode<T>(string name, INode<T> node)
+        {
+            nodes[name] = node;
+        }
+
+        public INode<T> GetNode<T>(string name)
+        {
+            if (!nodes.ContainsKey(name))
+                return null;
+
+            return nodes[name] as INode<T>;
+        }
+    }
+}
