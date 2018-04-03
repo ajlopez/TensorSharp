56ac1c1f271eb0dee20d9590061d57ab3bd66ca7
ajlopez <webmaster@ajlopez.com>
2018-04-03 16:04:52 -0300

    Session.Run first simple test

---
+++ Src/TensorSharp.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace TensorSharp.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SessionTests
+    {
+        [TestMethod]
+        public void SessionRunConstant()
+        {
+            var constant = Flow.Constant(42);
+            Session session = new Session();
+
+            var result = session.Run(constant);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
--- Src/TensorSharp/Session.cs
+++ Src/TensorSharp/Session.cs
@@ -4,8 +4,13 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using TensorSharp.Nodes;
 
     public class Session
     {
+        public INode<T> Run<T>(INode<T> node)
+        {
+            return node;
+        }
     }
 }
