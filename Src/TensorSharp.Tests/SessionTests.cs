c719fde259d26855e999388889808be1730ed4dc
ajlopez <webmaster@ajlopez.com>
2018-04-07 10:47:00 -0300

    Additional test, Session.Run

--- Src/TensorSharp.Tests/SessionTests.cs
+++ Src/TensorSharp.Tests/SessionTests.cs
@@ -30,6 +30,38 @@
             var result = session.Run(placeholder, context);
 
             Assert.IsNotNull(result);
+            Assert.AreEqual(42, result.GetValue(0));
+        }
+
+        [TestMethod]
+        public void SessionRunManyTimes()
+        {
+            var constant = Flow.Constant(1);
+            var placeholder = Flow.PlaceHolder<int>("number", new int[0]);
+            var operation = Flow.Add(placeholder, Flow.Constant(1));
+
+            Context context = new Context();
+            context.SetNode("number", constant);
+            Session session = new Session();
+
+            var result = session.Run(operation, context);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.GetValue(0));
+            
+            context.SetNode("number", result);
+
+            result = session.Run(operation, context);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.GetValue(0));
+
+            context.SetNode("number", result);
+
+            result = session.Run(operation, context);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.GetValue(0));
         }
     }
 }
